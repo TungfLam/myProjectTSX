f7472c94f5a4190e1f9b381b8ae929a494dc3f17
Tung Lam <lamptph20282@fpt.edu.vn>
2024-05-28 21:03:20 +0700

    update

--- screens/Cart.jsx
+++ screens/Cart.jsx
@@ -1,11 +1,39 @@
-import React, {useState} from 'react';
+import React, { useState, useEffect } from 'react';
 import { View, Text, Button, StyleSheet } from 'react-native';
+import * as Haptics from 'expo-haptics';
+import io from 'socket.io-client';
+
 
 export default function Cart() {
 
+
+
   const [count, setCount] = useState(0);
 
-
+  const handlePressSuccess = () => {
+    setCount(count + 1);
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Success);
+  };
+  const handlePressWarning = () => {
+    setCount(count + 1);
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Warning);
+  };
+  const handlePressError = () => {
+    setCount(count + 1);
+    Haptics.notificationAsync(Haptics.NotificationFeedbackType.Error);
+  };
+  const handlePressHeavy = () => {
+    setCount(count + 1);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Heavy);
+  };
+  const handlePressLight = () => {
+    setCount(count + 1);
+    Haptics.impactAsync(Haptics.ImpactFeedbackStyle.Light);
+  };
+  const handlePressNormal = () => {
+    setCount(count + 1);
+    Haptics.selectionAsync();
+  };
 
 
   return (
@@ -13,8 +41,28 @@ export default function Cart() {
       <Text>You clicked {count} times</Text>
 
       <Button
-        onPress={() => setCount(count + 1)}
-        title="Click me!"
+        onPress={handlePressSuccess}
+        title="Success"
+      />
+      <Button
+        onPress={handlePressWarning}
+        title="Warning"
+      />
+      <Button
+        onPress={handlePressError}
+        title="Error"
+      />
+      <Button
+        onPress={handlePressHeavy}
+        title="Heavy"
+      />
+      <Button
+        onPress={handlePressLight}
+        title="Light"
+      />
+      <Button
+        onPress={handlePressNormal}
+        title="Normal"
       />
     </View>
   );
@@ -25,8 +73,8 @@ export default function Cart() {
 
 const styles = StyleSheet.create({
   container: {
-    flex: 1, 
-    justifyContent: 'center', 
+    flex: 1,
+    justifyContent: 'center',
     alignItems: 'center',
   },
 });
